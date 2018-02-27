757bb292e471461712ad308fd637510b1b8ba060
Mia Hawkins <ilearnarcheology@aol.com>
2018-02-27 11:31:25 -0600

    add initial components

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () =>
+  <div className= "navbar">
+    <h1>The Project Portal</h1>
+    <h3>Handy helpers at work.</h3>
+    <div id="nav">
+      
+    </div>
+  </div>;
+
+export default Nav;
