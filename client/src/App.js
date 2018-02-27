b7fc45fb5c8e80e3daabbb6341d9f2f7d3ab26a0
Mia Hawkins <ilearnarcheology@aol.com>
2018-02-27 12:06:00 -0600

    add nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import TodoItem from './components/TodoItem/TodoItem';
+import TodoList from './components/TodoList/TodoList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nav />
+        <Wrapper>
+          <TodoList
+
+          />
+        </Wrapper>
       </div>
     );
   }
