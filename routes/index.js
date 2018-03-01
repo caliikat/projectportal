6ca467f5610c2cafc45a553caf703785ed1abeb2
Mia Hawkins <ilearnarcheology@aol.com>
2018-03-01 15:22:46 -0600

    add routes

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,20 @@ router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+app.get("/", (req, res) => {
+  var myData = new Todo(req.body);
+  myData.render()
+});
+
+app.post("/", (req, res) => {
+  var myData = new Todo(req.body);
+  myData.save()
+    .then(item => {
+      res.send("item saved to database");
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
+});
+
 module.exports = router;
